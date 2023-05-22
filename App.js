e4b125620e1c7ab1d0002048ea0a162a2ed3310d
Alexey Lisovoy <88471482+1leha@users.noreply.github.com>
2023-05-22 08:52:15 +0300

    adding nav provider

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
+import "react-native-gesture-handler";
 import { useFonts } from "expo-font";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { RegistrationScreen } from "./Screens/RegistrationScreen";
 import { LoginScreen } from "./Screens/LoginScreen";
 import { PostsScreen } from "./Screens/PostsScreen";
 import { Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -16,11 +19,20 @@ export default function App() {
     return null;
   }
 
+  const MainNav = createStackNavigator();
+
   return (
-    <SafeAreaProvider>
-      {/* <RegistrationScreen /> */}
-      {/* <LoginScreen /> */}
-      <PostsScreen />
-    </SafeAreaProvider>
+    <NavigationContainer>
+      <SafeAreaProvider>
+        <MainNav.Navigator initialRouteName="Registration">
+          <MainNav.Screen
+            name="Registration"
+            component={<RegistrationScreen />}
+          />
+          <MainNav.Screen name="Login" component={<LoginScreen />} />
+          <MainNav.Screen name="Main" component={<PostsScreen />} />
+        </MainNav.Navigator>
+      </SafeAreaProvider>
+    </NavigationContainer>
   );
 }
